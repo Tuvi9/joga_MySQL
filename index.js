1e1c0599438e992ed4945f22b2b1f38bbaa57a6d
Tuvi9 <kaspar.pavel@voco.ee>
2023-11-27 11:26:07 +0200

    Display an article by slug.

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ app.get('/', (req, res) => {
     })
 });
 
+// show article by slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result;
+        console.log(article);
+        res.render('article', {article:article});
+    });
+});
 // app start point
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
